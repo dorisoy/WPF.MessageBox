ea464c264db63afd2d4c646272d9f1e73c0ef87c
Pierre <pierre@delange.design>
2022-03-22 10:40:00 -0700

    Add project files.

---
+++ MessageBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Utilities
+{
+    /// <summary>
+    /// Interaction logic for MessageBox.xaml
+    /// </summary>
+    public partial class MessageBox : Window
+    {
+        public MessageBox()
+        {
+            InitializeComponent();
+        }
+    }
+    public enum MessageBoxType
+    {
+        Critical,
+        Warning,
+        Information,
+        Question
+    }
+
+    public enum MessageBoxButtons
+    {
+        Ok,
+        Cancel,
+        OkCancel,
+        YesNo,
+        YesNoCancel
+    }
+}
